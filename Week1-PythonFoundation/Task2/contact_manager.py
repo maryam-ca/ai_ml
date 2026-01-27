957b443605c1e0f7d7fce369cc15f4aad0b47b5c
Maryam Fatima <mminhas1405@gmail.com>
2026-01-27 00:24:35 -0800

    Update contact_manager.py

--- Week1-PythonFoundation/Task2/contact_manager.py
+++ Week1-PythonFoundation/Task2/contact_manager.py
@@ -1,43 +1,37 @@
 # contact_manager.py
 # File-based Contact Manager
 
-FILE_NAME = "contacts.txt"
+FILE = "contacts.txt"
 
 
 def add_contact(name, phone):
     try:
-        with open(FILE_NAME, "a") as file:
-            file.write(f"{name},{phone}\n")
-        print("Contact added successfully.")
+        # Append mode
+        with open(FILE, "a") as f:
+            f.write(f"{name},{phone}\n")
+        print("Contact added")
     except Exception as e:
         print("Error:", e)
 
 
 def view_contacts():
     try:
-        with open(FILE_NAME, "r") as file:
-            contacts = file.readlines()
-            if not contacts:
-                print("No contacts found.")
-            for contact in contacts:
-                name, phone = contact.strip().split(",")
+        # Read mode
+        with open(FILE, "r") as f:
+            for line in f:
+                name, phone = line.strip().split(",")
                 print(f"Name: {name}, Phone: {phone}")
     except FileNotFoundError:
-        print("No contact file found.")
+        print("No contacts found")
 
 
 def main():
     while True:
-        print("\n1. Add Contact")
-        print("2. View Contacts")
-        print("3. Exit")
-
-        choice = input("Choose option: ")
+        print("\n1 Add Contact\n2 View Contacts\n3 Exit")
+        choice = input("Choice: ")
 
         if choice == "1":
-            name = input("Enter name: ")
-            phone = input("Enter phone: ")
-            add_contact(name, phone)
+            add_contact(input("Name: "), input("Phone: "))
         elif choice == "2":
             view_contacts()
         elif choice == "3":
@@ -46,4 +40,5 @@ def main():
             print("Invalid choice")
 
 
-main()
+if __name__ == "__main__":
+    main()
